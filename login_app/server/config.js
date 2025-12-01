b4ca369e4176035b9aef5fd1c54fedcdd6fd0b28
Rahma Shahbaz <samisk3232@gmail.com>
2025-11-30 22:27:18 -0800

    Update config.js

--- login_app/server/config.js
+++ login_app/server/config.js
@@ -1,7 +1,8 @@
+// config.js
 export default {
-  JWT_SECRET: "AbdulRehmanSecretKey123",
-  JWT_REFRESH_SECRET: "AbdulRehmanRefreshSecret456",
-  EMAIL: "damion.ziemann61@ethereal.email",
-  PASSWORD: "Mbf1QN9nuA6mpxyddn",
-  ATLAS_URI: "mongodb+srv://Abdul_Rehman:0M3Ca46C8j7QIKCl@cluster0.2h5xtzm.mongodb.net/login_app?retryWrites=true&w=majority&appName=Cluster0"
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+  EMAIL: process.env.EMAIL,
+  PASSWORD: process.env.PASSWORD,
+  ATLAS_URI: process.env.ATLAS_URI,
 };
